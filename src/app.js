ec2b62588953cc526c7b7f41c1436d9973d42111
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-17 01:15:02 +0200

    element class

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import { searchMovies, getMovieById } from "./api.js";
+
+const el = {
+    form: document.getElementById('searchForm'),
+    q: document.getElementById('q'),
+    resultsGrid: document.getElementById('resultsGrid'),
+    pager: document.getElementById('pager'),
+    prev: document.getElementById('prevBtn'),
+    next: document.getElementById('nextBtn'),
+    pageInfo: document.getElementById('pageInfo'),
+    showResultsBtn: document.getElementById('showResultsBtn'),
+    showFavsBtn: document.getElementById('showFavsBtn'),
+    favoritesView: document.getElementById('favoritesView'),
+    favList: document.getElementById('favList'),
+    clearFavBtn: document.getElementById('clearFavBtn'),
+    helpBtn: document.getElementById('helpBtn'),
+    cardTpl: document.getElementById('cardTpl'),
+    modal: document.getElementById('modal'),
+    modalContent: document.getElementById('modalContent'),
+};
+
